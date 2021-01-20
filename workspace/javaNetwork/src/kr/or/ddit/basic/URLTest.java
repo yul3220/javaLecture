aa6f885907492e6ef1b6a43f08fb3b1285571b81
yul3220 <jyr3220@gmail.com>
2021-01-20 20:54:39 +0900

    2021.01.20

---
+++ workspace/javaNetwork/src/kr/or/ddit/basic/URLTest.java
@@ -0,0 +1,24 @@
+package kr.or.ddit.basic;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class URLTest {
+	public static void main(String[] args) throws MalformedURLException {
+		// URL클래스 ==> 인터넷에 존재하는 서버들의 자원에 접근할 수 있는 주소를 다루는 클래스
+		
+		// http://ddit.or.kr:80/index.html?ttt=12>
+		
+		//URL url = new URL("http://ddit.or.kr:80/index.html?ttt=12>");
+		URL url = new URL("http", "ddit.or.kr", 80, "index.html?ttt=12>");
+		//프로토콜, 도메인, 포트번호, 가져올 자원
+		
+		System.out.println("Protocol : " + url.getProtocol());
+		System.out.println("Host : " + url.getHost());
+		System.out.println("Port : " + url.getPort());
+		System.out.println("File : " + url.getFile());
+		System.out.println("Path : " + url.getPath());
+		System.out.println("Query : " + url.getQuery());
+		System.out.println();
+	}
+}
