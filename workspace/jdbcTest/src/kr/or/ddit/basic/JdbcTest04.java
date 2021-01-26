59a21cfc9214ec794c6d5b63325d9a527a24b259
yul3220 <jyr3220@gmail.com>
2021-01-26 18:09:14 +0900

    2021.01.26

--- workspace/jdbcTest/src/kr/or/ddit/basic/JdbcTest04.java
+++ workspace/jdbcTest/src/kr/or/ddit/basic/JdbcTest04.java
@@ -12,15 +12,13 @@ import java.util.Scanner;
  	 DB의 Member테이블에서 회원의 주소에 입력한 값이 포함되는 데이터를
  	 모두 출력하세요.(아이디, 이름, 우편번호, 주소)
  */
+
 public class JdbcTest04 {
 	public static void main(String[] args) {
 		Connection conn = null;
 		Statement stmt = null;
 		ResultSet rs = null;
 		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("도, 시, 군, 구중 하나를 입력하세요  : ");
-		String addr = sc.nextLine();
 		
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
@@ -30,21 +28,24 @@ public class JdbcTest04 {
 					"JYR03",
 					"java");
 			
-			String sql = "select mem_id, mem_name, mem_zip, mem_add1 ||' '|| mem_add2 as address "
-					+ "from member "
-					+ "where mem_add1 like '%" + addr +"%'";
+			System.out.print("검색할 주소 입력  : ");
+			String addr = sc.nextLine();
+			
+			String sql = "select mem_id, mem_name, mem_zip, mem_add1 ||' '|| mem_add2 as address"
+					+ " from member"
+					+ " where mem_add1 like '%" + addr +"%'";
 			
 			stmt = conn.createStatement();
 			
 			rs = stmt.executeQuery(sql);
 			
-			System.out.println("실행할 sql문 : " + sql);
+			System.out.println();
 			System.out.println(" == 쿼리문 처리 결과 == ");
 			while(rs.next()){
-				System.out.println("mem_id : " + rs.getString("mem_id"));
-				System.out.println("mem_name : " + rs.getString("mem_name"));
-				System.out.println("mem_zip : " + rs.getString("mem_zip"));
-				System.out.println("mem_addr : " + rs.getString("address"));
+				System.out.println("아이디 : " + rs.getString("mem_id"));
+				System.out.println("이름 : " + rs.getString("mem_name"));
+				System.out.println("우편번호 : " + rs.getString("mem_zip"));
+				System.out.println("주소 : " + rs.getString("address"));
 				System.out.println("----------------------------------");
 			}
 			System.out.println("출력 끝.....");	
